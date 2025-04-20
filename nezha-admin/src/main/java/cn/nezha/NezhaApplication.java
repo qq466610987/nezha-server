f10950424bb21f63fa63abb0fd7a120be54552cd
46661 <466610987@qq.com>
2025-04-20 11:59:04 +0800

    初始化项目

---
+++ nezha-admin/src/main/java/cn/nezha/NezhaApplication.java
@@ -0,0 +1,13 @@
+package cn.nezha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NezhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NezhaApplication.class, args);
+    }
+
+}
---
+++ nezha-admin/src/test/java/cn/nezha/NezhaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nezha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NezhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
